96b256319b773ca3a4baf29db4426576862666a1
isaaczinda <me@isaaczinda.com>
2019-10-24 10:33:26 -0700

    add integration tests

--- rust/src/tests.rs
+++ rust/src/tests.rs
@@ -1,24 +1,29 @@
-// support synchronous websockets, great for testing
-extern crate websocket;
 
-
-
-
-// use ws::{connect, CloseCode};
-//
-// ws::connect("ws://127.0.0.1:3012", |out| {
-//     out.send("Hello WebSocket").unwrap();
-//
-//     move |msg| {
-//         println!("Got message: {}", msg);
-//         out.close(CloseCode::Normal)
-//     }
-// }).unwrap()
 
 #[cfg(test)]
 mod tests {
     use crate::next_piece;
     use websocket::ClientBuilder;
+
+    // support synchronous websockets, great for testing
+    extern crate websocket;
+    use std::{thread, time};
+    use std::sync::{Once};
+
+    static INIT_SERVER : Once = Once::new();
+
+    fn start_server() {
+        use crate::main as program_main;
+
+        // this is synchronous, so we'll wait the 1 second before returning
+        INIT_SERVER.call_once(|| {
+            thread::spawn(move || {
+                program_main();
+            });
+
+            thread::sleep(time::Duration::from_millis(1000));
+        });
+    }
 
     #[test]
     fn test_next_piece() {
@@ -29,13 +34,16 @@ mod tests {
     // test to make sure initialize code is good!
     #[test]
     fn test_ws_init_flow() {
+        start_server();
+
         let mut client = ClientBuilder::new("ws://127.0.0.1:3012")
             .unwrap()
             .connect_insecure()
             .unwrap();
 
-
         let msg = client.recv_message().unwrap();
+
+        println!("message received");
 
         let message_received = match msg {
             OwnedMessage => true,
