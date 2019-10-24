19e3a1b275fc4d96ca5fbca8cdab53dbc4b25d30
isaaczinda <me@isaaczinda.com>
2019-10-24 11:19:47 -0700

    add test for init flow

--- rust/src/tests.rs
+++ rust/src/tests.rs
@@ -31,9 +31,15 @@ mod tests {
         assert!(next_piece() <= 7);
     }
 
-    // test to make sure initialize code is good!
+    /*
+    Test to make sure that initial response from server is properly formatted.
+    */
     #[test]
     fn test_ws_init_flow() {
+        use websocket::message::OwnedMessage;
+        use serde_json::{Result, Error, Value};
+
+
         start_server();
 
         let mut client = ClientBuilder::new("ws://127.0.0.1:3012")
@@ -43,14 +49,28 @@ mod tests {
 
         let msg = client.recv_message().unwrap();
 
-        println!("message received");
-
-        let message_received = match msg {
-            OwnedMessage => true,
-            WebSocketError => false,
+        // assert that a message was received
+        let message_string = match msg {
+            OwnedMessage::Text(text) => text,
+            WebSocketError => panic!("Can't read message."),
         };
 
-        assert!(message_received);
+        // Parse the string of data into serde_json::Value.
+        let message_json : Value = serde_json::from_str(&message_string).unwrap();
+
+        // check to make sure message_type is correct
+        assert!(message_json["type"] == "init");
+
+        // check to make sure piece_type is in correct range
+        assert!(message_json["piece_type"].is_number());
+        let piece_type : i64 = message_json["piece_type"].as_i64().unwrap();
+        assert!(piece_type >= 0);
+        assert!(piece_type <= 6);
+
+        // check to make sure we have a non-zero user id
+        assert!(message_json["player_id"].is_number());
+        let user_id = message_json["player_id"].as_i64().unwrap();
+        assert!(user_id >= 0);
     }
 }
 
